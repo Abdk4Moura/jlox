87c525118cd1198724e9937ec84c68415607da96
kolawole <pro.kaiserlautern@gmail.com>
2023-04-15 15:47:05 +0100

    all the changes including in the roadmap.org

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -1,9 +1,9 @@
 /*
  *@title: Boring generation of boring boilerplate code
- *
+ *@created: <2023-04-15 Sat>
  **/
 
-package com.craftinginterpreters.tool;
+package tool;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -46,6 +46,8 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
@@ -53,8 +55,22 @@ public class GenerateAst {
             defineType(writer, baseName, className, fields);
         }
 
+        // The base accept() method.
+        writer.println();
+        writer.println("   abstract <R> R accept(Vistor<R> visitor)");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("   interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + " (" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("  }");
     }
 
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -71,6 +87,12 @@ public class GenerateAst {
         }
 
         writer.println("}");
+
+        writer.println();
+        writer.println("     @Override");
+        writer.println("     <R> R accept (Vistor<R> visitor) {");
+        writer.println("        return visitor.visit" + className + baseName + "(this);");
+        writer.println("    }");
 
         // Fields.
         writer.println();
